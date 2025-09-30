90d5cffc670156a4117f813f553f7223bf14e6f8
erin4ei <142532773+erin4ei@users.noreply.github.com>
2025-09-30 14:42:32 -0700

    In Class Activity
    09/30/25

---
+++ script.js
@@ -0,0 +1,35 @@
+// For this section, use JS to select the element here by its ID and add to the innerHTML a span with a class of "tan" and text inside that span that says "This is new tan text"
+document.getElementById("firstPara").innerHTML += "<span class='tan'> This is new tan text.</span>";
+
+// For this section, select all of these images and change their width from its current setting to 250 pixels
+let images = document.getElementsByTagName("img");
+console.log(images);
+
+for(let img of images){
+    img.width = "250";
+}
+
+// For the elements below, there are two class names. You will need to select the groups of elements separately, and for the elements with the class "med-blue", add inline styles to change the text color to #3C5E73. For the elements with the class "light-blue" change the text color to #7C9EA6. You can access the inline styles for an element with the dot operator (.) and the attribute name "style" followed by another dot and the style you want to set (in this case, element.style.color).
+
+let lightBlueSpans = document.getElementsByClassName("med-blue");
+let medBlueSpans = document.getElementsByClassName("light-blue");
+
+for(let span of medBlueSpans){
+    span.style.color = "#3C5E73";
+}
+
+for(let span of lightBlueSpans){
+    span.style.color = "#7C9EA6";
+}
+
+// Let's select some SVG icons by a CSS selector and change the stroke color on these. For fun, let's create an array of the colors used in our document (find these in the root of the CSS document), and as we iterate through our icons, let's change each one to one of the colors in that array of colors from the document.
+
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+let svgs = document.querySelectorAll("#svgs svg");
+for(let i = 0; i < colors.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+// In this section, select the first of the spans with the class "bold" and change the text color to the light blue color from our CSS (#7C9EA6).
+
+document.querySelector(".bold").style.color="#7C9EA6";
